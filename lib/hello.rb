193d7f500e473105c0a9104eab9fee1b810e95b4
Brandon  Harris <cheeridon@gmail.com>
2017-09-28 01:03:02 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,10 +1,14 @@
 def hello_t array
-  idx = 0
-  while idx < array.length
-    yield array[idx]
-    idx += 1
-  end
-  array 
+  if block_given?
+    idx = 0
+    while idx < array.length
+      yield array[idx]
+      idx += 1
+    end
+    array
+  else
+    puts "Hey! No block was given!"
+  end 
 end
 
 hello_t(["Tim", "Tom", "Jim"]) do |name|
